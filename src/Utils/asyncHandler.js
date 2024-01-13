484b5c2227129b9676e876c8fa953212e9ef619d
Raj sahani <Rajsahaniofficial19@gmail.com>
2024-01-13 22:01:57 +0530

    custom api responce and error handling

---
+++ src/Utils/asyncHandler.js
@@ -0,0 +1,21 @@
+// promis method of a high order function
+const asyncHandler = (requestHandlerFn) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandlerFn(req, res, next)).catch((err) => {
+      next(err);
+    });
+  };
+};
+
+// try cath method
+
+// const asyncHandler = (rhFun) => async (req, res, next) => {
+//   try {
+//     await rhFun(req, res, next);
+//   } catch (error) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   }
+// };
--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
-import express from "express";
 import ConnectDb from "./Db/DbConnection.js";
 import dotenv from "dotenv";
-
+import { app } from "./app.js";
 dotenv.config();
-
-const app = express();
 
 ConnectDb()
   .then(() => {
     app.listen(process.env.PORT || 8000, () => {
       console.log(`Server is running on port:${process.env.PORT}`);
+    });
+    app.on("error", (error) => {
+      console.log("ERROR: ", error);
+      throw error;
     });
   })
   .catch((err) => {
