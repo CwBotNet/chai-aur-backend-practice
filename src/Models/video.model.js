e38f1b2cf44f05cb5120c54afb33e147dbd95766
Raj sahani <Rajsahaniofficial19@gmail.com>
2024-01-17 04:00:33 +0530

    model creation done

---
+++ src/Models/video.model.js
@@ -0,0 +1,44 @@
+import mongoose, { Schema } from "mongoose";
+
+const videoSchema = new Schema(
+  {
+    videFile: {
+      type: String, // cloudinary url
+      required: true,
+    },
+    thumbnail: {
+      type: String, // cloudianry url
+      required: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+  },
+
+  {
+    timestamps: true,
+  }
+);
+
+export const Video = mongoose.model("Video", videoSchema);
